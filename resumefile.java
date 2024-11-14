cece993627a6954abc57a67cfc9e69821714a7ae
pet147 <143010876+pet147@users.noreply.github.com>
2024-11-14 22:50:28 +0000

    Update resumefile.java

--- resumefile.java
+++ resumefile.java
@@ -1,10 +1,11 @@
+Java.utils.*
 class Test{
   public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);
 sc.nextInt();
     int num1 = sc.nextInt();
 
-    int num5 = num1 * num1++
+    int num5 = num1 * num1++;
     System.out.println(num5);
     System.out.println(num1);
   }
